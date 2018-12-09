dbb7aa88fab1e2e8f7de5fea65d27022a50d85ba
Pau Andrio <andriopau@gmail.com>
2018-12-09 23:34:44 +0100

    Genion bug fix

--- biobb_adapters/biobb_md/pycompss/gromacs/genion_pc.py
+++ biobb_adapters/biobb_md/pycompss/gromacs/genion_pc.py
@@ -6,7 +6,7 @@ from biobb_common.tools import file_utils as fu
 from biobb_md.gromacs import genion
 
 @task(input_tpr_path=FILE_IN, output_gro_path=FILE_OUT, input_top_zip_path=FILE_IN, output_top_zip_path=FILE_OUT)
-def pdb2gmx_pc(input_tpr_path, output_gro_path, input_top_zip_path, output_top_zip_path, properties, **kwargs):
+def genion_pc(input_tpr_path, output_gro_path, input_top_zip_path, output_top_zip_path, properties, **kwargs):
     try:
         genion.Genion(input_tpr_path=input_tpr_path, output_gro_path=output_gro_path, input_top_zip_path=input_top_zip_path, output_top_zip_path=output_top_zip_path, properties=properties, **kwargs).launch()
     except Exception:
