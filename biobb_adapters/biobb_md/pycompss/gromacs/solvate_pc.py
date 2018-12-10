516843e889385c877f0007f9de499de8ebc44e0b
Pau Andrio <andriopau@gmail.com>
2018-12-10 08:27:05 +0100

    Solvate bux fix

--- biobb_adapters/biobb_md/pycompss/gromacs/solvate_pc.py
+++ biobb_adapters/biobb_md/pycompss/gromacs/solvate_pc.py
@@ -6,7 +6,7 @@ from biobb_common.tools import file_utils as fu
 from biobb_md.gromacs import solvate
 
 @task(input_solute_gro_path=FILE_IN, output_gro_path=FILE_OUT, input_top_zip_path=FILE_IN, output_top_zip_path=FILE_OUT)
-def solvate_pc(iinput_solute_gro_path, output_gro_path, input_top_zip_path, output_top_zip_path, properties, **kwargs):
+def solvate_pc(input_solute_gro_path, output_gro_path, input_top_zip_path, output_top_zip_path, properties, **kwargs):
     try:
         solvate.Solvate(input_solute_gro_path=input_solute_gro_path, output_gro_path=output_gro_path, input_top_zip_path=input_top_zip_path, output_top_zip_path=output_top_zip_path, properties=properties, **kwargs).launch()
     except Exception:
