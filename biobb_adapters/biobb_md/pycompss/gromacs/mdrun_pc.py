28ba3fdbd521f18e85c08c4d1be1abdbc67c08c5
Pau Andrio <andriopau@gmail.com>
2018-12-10 12:05:09 +0100

    Facking outputs on mdrun

--- biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
+++ biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
@@ -4,12 +4,22 @@ from pycompss.api.constraint import constraint
 from pycompss.api.parameter import FILE_IN, FILE_OUT
 from biobb_common.tools import file_utils as fu
 from biobb_md.gromacs import mdrun
+import os
 
 @task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT, output_xtc_path=FILE_OUT)
 def mdrun_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path,
              output_log_path, output_xtc_path, properties, **kwargs):
     try:
         mdrun.Mdrun(input_tpr_path=input_tpr_path, output_trr_path=output_trr_path, output_gro_path=output_gro_path, output_edr_path=output_edr_path, output_log_path=output_log_path, output_xtc_path=output_xtc_path, properties=properties, **kwargs).launch()
+        if not os.path.exists(output_trr_path):
+            fu.write_failed_output(output_trr_path)
+        if not os.path.exists(output_edr_path):
+            fu.write_failed_output(output_edr_path)
+        if not os.path.exists(output_log_path):
+            fu.write_failed_output(output_log_path)
+        if not os.path.exists(output_xtc_path):
+            fu.write_failed_output(output_xtc_path)
+
     except Exception:
         traceback.print_exc()
         fu.write_failed_output(output_trr_path)
