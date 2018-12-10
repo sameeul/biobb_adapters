4deefae086a700a6778596123423d486f0e63c38
Pau Andrio <andriopau@gmail.com>
2018-12-10 16:06:33 +0100

    Removing xtc from mdrun_pc and mdrun_cpt_pc

--- biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
+++ biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
@@ -7,19 +7,17 @@ from biobb_md.gromacs import mdrun
 import os
 
 @constraint(ComputingUnits=2)
-@task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT, output_xtc_path=FILE_OUT)
+@task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT)
 def mdrun_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path,
-             output_log_path, output_xtc_path, properties, **kwargs):
+             output_log_path, properties, **kwargs):
     try:
-        mdrun.Mdrun(input_tpr_path=input_tpr_path, output_trr_path=output_trr_path, output_gro_path=output_gro_path, output_edr_path=output_edr_path, output_log_path=output_log_path, output_xtc_path=output_xtc_path, properties=properties, **kwargs).launch()
+        mdrun.Mdrun(input_tpr_path=input_tpr_path, output_trr_path=output_trr_path, output_gro_path=output_gro_path, output_edr_path=output_edr_path, output_log_path=output_log_path, properties=properties, **kwargs).launch()
         if not os.path.exists(output_trr_path):
             fu.write_failed_output(output_trr_path)
         if not os.path.exists(output_edr_path):
             fu.write_failed_output(output_edr_path)
         if not os.path.exists(output_log_path):
             fu.write_failed_output(output_log_path)
-        if not os.path.exists(output_xtc_path):
-            fu.write_failed_output(output_xtc_path)
 
     except Exception:
         traceback.print_exc()
@@ -27,4 +25,3 @@ def mdrun_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path,
         fu.write_failed_output(output_gro_path)
         fu.write_failed_output(output_edr_path)
         fu.write_failed_output(output_log_path)
-        fu.write_failed_output(output_xtc_path)
