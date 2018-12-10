b46a3cf2c39c938f03decd9a8d9fe3d4d1bf795d
Pau Andrio <andriopau@gmail.com>
2018-12-10 16:36:26 +0100

    ComputingUnits=4 for mdrun

--- biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
+++ biobb_adapters/biobb_md/pycompss/gromacs/mdrun_pc.py
@@ -6,7 +6,7 @@ from biobb_common.tools import file_utils as fu
 from biobb_md.gromacs import mdrun
 import os
 
-@constraint(ComputingUnits=2)
+@constraint(ComputingUnits=4)
 @task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT)
 def mdrun_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path,
              output_log_path, properties, **kwargs):
