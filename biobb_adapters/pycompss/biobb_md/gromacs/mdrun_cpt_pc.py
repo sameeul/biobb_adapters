96b9c970dc12eea277d8c619811bf3b5ed6dfafb
Jorge Ejarque <jorge.ejarque@bsc.es>
2019-05-08 15:41:13 +0200

    added multinode and constraints decorators

--- biobb_adapters/pycompss/biobb_md/gromacs/mdrun_cpt_pc.py
+++ biobb_adapters/pycompss/biobb_md/gromacs/mdrun_cpt_pc.py
@@ -1,19 +1,30 @@
 import traceback
-#from pycompss.api.task import task
-#from pycompss.api.constraint import constraint
-#from pycompss.api.parameter import FILE_IN, FILE_OUT
+from pycompss.api.task import task
+from pycompss.api.constraint import constraint
+from pycompss.api.multinode import multinode
+from pycompss.api.parameter import FILE_IN, FILE_OUT
 from biobb_common.tools import file_utils as fu
 from biobb_md.gromacs import mdrun
 import os
 
-#@task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT)
-def mdrun_cpt_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path, output_log_path, output_cpt_path, properties, **kwargs):
+@constraint(computingUnits="48")
+@multinode(computingNodes="1")
+@task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_xtc_path=FILE_OUT, output_log_path=FILE_OUT)
+def mdrun_cpt_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_path, output_xtc_path, output_log_path, output_cpt_path, properties, **kwargs):
+    num_nodes = int(os.environ["COMPSS_NUM_NODES"])
+    print("Received Num node: " + str(num_nodes))
+    num_threads = int(os.environ["COMPSS_NUM_THREADS"])
+    print("Received Num Threads: " + str(num_threads))
+    hostnames = os.environ["COMPSS_HOSTNAMES"]
+    print("Received Hostname: " + hostnames)  
     try:
-        mdrun.Mdrun(input_tpr_path=input_tpr_path, output_trr_path=output_trr_path, output_gro_path=output_gro_path, output_edr_path=output_edr_path, output_log_path=output_log_path, output_cpt_path=output_cpt_path, properties=properties, **kwargs).launch()
+        mdrun.Mdrun(input_tpr_path=input_tpr_path, output_trr_path=output_trr_path, output_gro_path=output_gro_path, output_edr_path=output_edr_path, output_xtc_path=output_xtc_path, output_log_path=output_log_path, output_cpt_path=output_cpt_path, properties=properties, **kwargs).launch()
         if not os.path.exists(output_trr_path):
             fu.write_failed_output(output_trr_path)
         if not os.path.exists(output_edr_path):
             fu.write_failed_output(output_edr_path)
+        if not os.path.exists(output_xtc_path):
+            fu.write_failed_output(output_xtc_path)
         if not os.path.exists(output_log_path):
             fu.write_failed_output(output_log_path)
 
@@ -22,5 +33,6 @@ def mdrun_cpt_pc(input_tpr_path, output_trr_path, output_gro_path, output_edr_pa
         fu.write_failed_output(output_trr_path)
         fu.write_failed_output(output_gro_path)
         fu.write_failed_output(output_edr_path)
+        fu.write_failed_output(output_xtc_path)
         fu.write_failed_output(output_log_path)
         fu.write_failed_output(output_cpt_path)
