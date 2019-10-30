daaaca4f81b7bc2a87858a11dcf9fec57ccac518
gbayarri <genis.bayarri@irbbarcelona.org>
2019-10-30 10:12:52 +0100

    Added CWL for chemistry > ambertools & babel

---
+++ biobb_adapters/cwl/biobb_chemistry/ambertools/reduce_add_hydrogens.cwl
@@ -0,0 +1,38 @@
+#!/usr/bin/env cwl-runner
+cwlVersion: v1.0
+class: CommandLineTool
+baseCommand: reduce_add_hydrogens
+hints:
+  DockerRequirement:
+    dockerPull: mmbirb/biobb_chemistry:latest
+inputs:
+  input_path:
+    type: File
+    inputBinding:
+      position: 1
+      prefix: --input_path
+
+  output_path:
+    type: string
+    inputBinding:
+      position: 2
+      prefix: --output_path
+    default: "output.pdb"
+
+  config:
+    type: string?
+    inputBinding:
+      position: 3
+      prefix: --config
+
+outputs:
+
+  output_file:
+    type: File
+    outputBinding:
+      glob: $(inputs.output_path)
+
+$namespaces:
+  edam: http://edamontology.org/
+$schemas:
+  - http://edamontology.org/EDAM_1.22.owl
--- biobb_adapters/cwl/biobb_chemistry/test/cwl_unitest.sh
+++ biobb_adapters/cwl/biobb_chemistry/test/cwl_unitest.sh
@@ -3,7 +3,15 @@
 RUNNER=cwltool
 
 BIOBB_CHEMISTRY=$HOME/projects/BioBB/biobb_adapters/biobb_adapters/cwl/biobb_chemistry
-$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_ac.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_ac.yml
-$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_cns.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_cns.yml
-$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_gmx.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_gmx.yml
-$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_gmx_opls.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_gmx_opls.yml
+#$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_ac.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_ac.yml
+#$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_cns.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_cns.yml
+#$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_gmx.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_gmx.yml
+#$RUNNER $BIOBB_CHEMISTRY/acpype/acpype_params_gmx_opls.cwl $BIOBB_CHEMISTRY/test/acpype/acpype_params_gmx_opls.yml
+
+#$RUNNER $BIOBB_CHEMISTRY/ambertools/reduce_add_hydrogens.cwl $BIOBB_CHEMISTRY/test/ambertools/reduce_add_hydrogens.yml
+#$RUNNER $BIOBB_CHEMISTRY/ambertools/reduce_remove_hydrogens.cwl $BIOBB_CHEMISTRY/test/ambertools/reduce_remove_hydrogens.yml
+
+#$RUNNER $BIOBB_CHEMISTRY/babelm/babel_add_hydrogens.cwl $BIOBB_CHEMISTRY/test/babelm/babel_add_hydrogens.yml
+#$RUNNER $BIOBB_CHEMISTRY/babelm/babel_remove_hydrogens.cwl $BIOBB_CHEMISTRY/test/babelm/babel_remove_hydrogens.yml
+#$RUNNER $BIOBB_CHEMISTRY/babelm/babel_convert.cwl $BIOBB_CHEMISTRY/test/babelm/babel_convert.yml
+$RUNNER $BIOBB_CHEMISTRY/babelm/babel_minimize.cwl $BIOBB_CHEMISTRY/test/babelm/babel_minimize.yml
