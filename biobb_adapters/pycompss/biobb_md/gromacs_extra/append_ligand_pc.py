bbc6e10aeda41ae1b86a79eac591693e165e529c
Pau Andrio <andriopau@gmail.com>
2020-01-21 19:28:53 +0100

    Adding append_ligand adapter

---
+++ biobb_adapters/pycompss/biobb_md/gromacs_extra/append_ligand_pc.py
@@ -0,0 +1,14 @@
+import traceback
+from pycompss.api.task import task
+from pycompss.api.constraint import constraint
+from pycompss.api.parameter import FILE_IN, FILE_OUT
+from biobb_common.tools import file_utils as fu
+from gromacs_extra import append_ligand
+
+@task(input_top_zip_path=FILE_IN, input_itp_path=FILE_IN, output_top_zip_path=FILE_OUT, input_posres_itp_path=FILE_IN, on_failure="IGNORE")
+def append_ligand_pc(input_top_zip_path, input_itp_path, output_top_zip_path, input_posres_itp_path, properties, **kwargs):
+    try:
+        append_ligand.AppendLigand(input_top_zip_path=input_top_zip_path, input_itp_path=input_itp_path, output_top_zip_path=output_top_zip_path, input_posres_itp_path=input_posres_itp_path, properties=properties, **kwargs).launch()
+    except Exception:
+        traceback.print_exc()
+        fu.write_failed_output(output_top_zip_path)
