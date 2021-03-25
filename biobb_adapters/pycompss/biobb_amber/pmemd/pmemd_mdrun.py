d42b9ae0bd199106f1ba4968bb63226b19e925c9
Pau Andrio <andriopau@gmail.com>
2021-03-25 22:41:09 +0100

    Feat(amber): Adding MPI to Amber

--- biobb_adapters/pycompss/biobb_amber/pmemd/pmemd_mdrun.py
+++ biobb_adapters/pycompss/biobb_amber/pmemd/pmemd_mdrun.py
@@ -5,19 +5,25 @@ import traceback
 # Pycompss
 from pycompss.api.task import task
 from pycompss.api.parameter import FILE_IN, FILE_OUT
+from pycompss.api.multinode import multinode
+from pycompss.api.constraint import constraint
 # Adapters commons pycompss
 from biobb_adapters.pycompss.biobb_commons import task_config
 # Wrapped Biobb
 from biobb_amber.pmemd.pmemd_mdrun import PmemdMDRun  # Importing class instead of module to avoid name collision
 
 task_time_out = int(os.environ.get('TASK_TIME_OUT', 0))
+computing_nodes = str(os.environ.get('TASK_COMPUTING_NODES', "1"))
+computing_units = str(os.environ.get('TASK_COMPUTING_UNITS', "1"))
 
 
-@task(input_top_path=FILE_IN, input_crd_path=FILE_IN, output_log_path=FILE_OUT, output_traj_path=FILE_OUT, output_rst_path=FILE_OUT, input_mdin_path=FILE_IN, input_cpin_path=FILE_IN, input_ref_path=FILE_IN, output_cpout_path=FILE_IN, output_cprst_path=FILE_IN, output_mdinfo_path=FILE_IN, 
+@constraint(computing_units=computing_units)
+@multinode(computing_nodes=computing_nodes)
+@task(input_top_path=FILE_IN, input_crd_path=FILE_IN, output_log_path=FILE_OUT, output_traj_path=FILE_OUT, output_rst_path=FILE_OUT, input_mdin_path=FILE_IN, input_cpin_path=FILE_IN, input_ref_path=FILE_IN, output_cpout_path=FILE_IN, output_cprst_path=FILE_IN, output_mdinfo_path=FILE_IN,
       on_failure="IGNORE", time_out=task_time_out)
 def _pmemdmdrun(input_top_path, input_crd_path, output_log_path, output_traj_path, output_rst_path, input_mdin_path, input_cpin_path, input_ref_path, output_cpout_path, output_cprst_path, output_mdinfo_path,  properties, **kwargs):
     
-    task_config.pop_pmi(os.environ)
+    task_config.config_multinode(properties)
     
     try:
         PmemdMDRun(input_top_path=input_top_path, input_crd_path=input_crd_path, output_log_path=output_log_path, output_traj_path=output_traj_path, output_rst_path=output_rst_path, input_mdin_path=input_mdin_path, input_cpin_path=input_cpin_path, input_ref_path=input_ref_path, output_cpout_path=output_cpout_path, output_cprst_path=output_cprst_path, output_mdinfo_path=output_mdinfo_path, properties=properties, **kwargs).launch()
--- biobb_adapters/pycompss/biobb_md/gromacs/mdrun.py
+++ biobb_adapters/pycompss/biobb_md/gromacs/mdrun.py
@@ -4,9 +4,9 @@ import sys
 import traceback
 # Pycompss
 from pycompss.api.task import task
+from pycompss.api.parameter import FILE_IN, FILE_OUT
 from pycompss.api.multinode import multinode
 from pycompss.api.constraint import constraint
-from pycompss.api.parameter import FILE_IN, FILE_OUT
 # Adapters commons pycompss
 from biobb_adapters.pycompss.biobb_commons import task_config
 # Wrapped Biobb
@@ -16,11 +16,11 @@ task_time_out = int(os.environ.get('TASK_TIME_OUT', 0))
 computing_nodes = str(os.environ.get('TASK_COMPUTING_NODES', "1"))
 computing_units = str(os.environ.get('TASK_COMPUTING_UNITS', "1"))
 
+
 @constraint(computing_units=computing_units)
 @multinode(computing_nodes=computing_nodes)
 @task(input_tpr_path=FILE_IN, output_trr_path=FILE_OUT, output_gro_path=FILE_OUT, output_edr_path=FILE_OUT, output_log_path=FILE_OUT, input_cpt_path=FILE_IN, output_xtc_path=FILE_IN, output_cpt_path=FILE_IN, output_dhdl_path=FILE_IN, 
       on_failure="IGNORE", time_out=task_time_out)
-
 def _mdrun(input_tpr_path, output_trr_path, output_gro_path, output_edr_path, output_log_path, input_cpt_path, output_xtc_path, output_cpt_path, output_dhdl_path,  properties, **kwargs):
     
     task_config.config_multinode(properties)
